274ffeb7599c82ce41d64c4dfaf404600c9cd385
Patrick Correia Landra <Patrickcorreialandra@gmail.com>
2023-11-19 22:53:09 -0300

    Adicionando construtor

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,12 @@ import animais.Cachorro;
 public class Main {
 
     public static void main(String[] args) {
-        Cachorro cachorro1;
-        cachorro1 = new Cachorro();
+        Cachorro cachorro1 = new Cachorro("Coringa","verde",50,80,"Normal","Com Fome");
+        Cachorro cachorro2 = new Cachorro("Bobb","Branco",1.8,80,"Feliz","Sastifeito");
 
-        cachorro1.nome = "Thor";
-        cachorro1.cor = "Branco e Preto";
-        cachorro1.altura = 1.25;
-        cachorro1.peso =  40.5;
+        System.out.println(cachorro2.toString());
+        System.out.println(cachorro1.toString());
 
-        cachorro1.latir();
-
-        System.out.println( "O cachorro pegou a " + cachorro1.pegar());
-
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro " + cachorro1.interagir("Vai dormir"));
-        System.out.println("O cachorro está " + cachorro1.interagir("Triste"));
     }
 
 }
--- src/animais/Cachorro.java
+++ src/animais/Cachorro.java
@@ -1,15 +1,76 @@
 package animais;
 
 public class Cachorro {
-    public String nome;
-    public  String cor;
-    public double altura;
 
-    public double peso;
-    public String estadoDeEspirito;
+    //Atributos
+    private String nome;
+    private  String cor;
+    private double altura;
 
-    public void comer(){
+    private double peso;
+    private String estadoDeEspirito;
 
+    private String comida;
+
+
+    // Construtores
+    public Cachorro(){}
+   public Cachorro(String nome, String cor, double altura, double peso, String estadoDeEspirito, String comida){
+        this.nome = nome;
+        this.cor = cor;
+        this. altura = altura;
+        this.peso = peso;
+        this.estadoDeEspirito = estadoDeEspirito;
+        this.comida = comida;
+    }
+
+    //Métodos
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public String getComida(){
+        return comida;
+    }
+
+    public void setComida(String comida) {
+        this.comida = comida;
     }
 
     public void latir() {
@@ -17,9 +78,21 @@ public class Cachorro {
     }
 
 
+    public String comer(String racao){
+        if (racao.equals("ração de carne"))
+         this.setComida("sastifeito");
+
+        else if (racao.equals("ração de frango")){
+            this.setComida("Bravo");
+        }
+
+        return comida;
+    }
+
     public  String pegar(){
         return "Bolinha";
     }
+
     public String interagir(String acao){
 //
         switch (acao){
